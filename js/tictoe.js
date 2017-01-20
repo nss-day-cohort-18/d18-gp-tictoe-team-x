0ea40f49e7182ee2e4e11cfce2c8fc11fbf0a83e
Jordan Nelson <jnelsontn@gmail.com>
2017-01-20 11:13:01 -0600

    testing a win

--- js/tictoe.js
+++ js/tictoe.js
@@ -18,3 +18,32 @@ function handleClass(event) {
 		this.classList.add("o");
 	}
 }
+
+/* var winningCombo = [
+    [1, 4, 7], x
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]; */
+function winloseLogic ()
+	// grab Elements and name 
+	var c1 = document.getElementById("1");
+	var c2 = document.getElementById("2");
+	var c3 = document.getElementById("3");
+	var c4 = document.getElementById("4");
+	var c5 = document.getElementById("5");
+	var c6 = document.getElementById("6");
+	var c7 = document.getElementById("7");
+	var c8 = document.getElementById("8");
+	var c9 = document.getElementById("9");
+	var x = "x";
+	var o = "o;"
+
+// X 
+	if (c1.classList.contains(x) && c4.classList.contains(x) && c7.classList.contains(x)) {
+		alert("you won");
+	}
